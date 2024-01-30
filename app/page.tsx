b3d29c93149f3e025e2f8bf3b9fccb811d6242b1
Yes2eul <yeseul5611@gmail.com>
2024-01-30 21:39:57 +0900

    feat: home init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import TestUi from "@/components/test/test";
-
 export default function Home() {
   return (
     <div className=" w-full h-screen flex justify-center items-center">
-      <TestUi></TestUi>
+      Super Time Home
     </div>
   );
 }
