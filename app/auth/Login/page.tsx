320a7e9c499ba7e4e5275dc55fef7e7ee7f510d3
Yes2eul <yeseul5611@gmail.com>
2024-01-30 18:10:24 +0900

    feat: style/module.css 파일 추가

--- app/auth/Login/page.tsx
+++ app/auth/Login/page.tsx
@@ -3,26 +3,25 @@
 import React from "react";
 import { Button, Input } from "@nextui-org/react";
 import Link from "next/link";
+import "../../../styles/login.module.css";
 
 export default function Login() {
   return (
-    <main>
-      <section>
-        <h1>로그인</h1>
-        <form>
-          <article>
+    <>
+      <header>로그인</header>
+      <main>
+        <section>
+          <form>
             <Input type="email" label="이메일" />
             <Input type="password" label="비밀번호" />
-          </article>
-          <article>
-            <Button className="bg-button_color text-white">로그인</Button>
-            <Button className="bg-button_color text-white">소셜 로그인</Button>
-          </article>
-        </form>
+          </form>
+          <Button className="bg-button_color text-white">로그인</Button>
+          <Button className="bg-button_color text-white">소셜 로그인</Button>
+        </section>
         <Link href="/auth/signup">
           <Button className="bg-button_color text-white">회원가입</Button>
         </Link>
-      </section>
-    </main>
+      </main>
+    </>
   );
 }
--- app/auth/SignUp/page.tsx
+++ app/auth/SignUp/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Button, Input } from "@nextui-org/react";
 import Link from "next/link";
+import "../../../styles/signup.module.css";
 
 interface DropdownItem {
   key: string;
@@ -11,26 +12,25 @@ interface DropdownItem {
 
 export default function SignUp() {
   return (
-    <main>
-      <section>
-        <h1>회원가입</h1>
-        <form>
-          <article>
+    <>
+      <header>회원가입</header>
+      <main>
+        <section>
+          <form>
             <Input type="email" label="이메일" />
             <Input type="text" label="이름" />
             <Input type="text" label="닉네임" />
-            <span>닉네임 중복 메세지</span>
+            <p>닉네임 중복 메세지</p>
             <Button>기수 선택</Button>
-
             <Input type="password" label="비밀번호" />
             <Input type="password" label="비밀번호 확인" />
-          </article>
+          </form>
           <Button className="bg-button_color text-white">회원가입</Button>
-        </form>
+        </section>
         <Link href="/auth/login">
           <Button className="bg-button_color text-white">로그인</Button>
         </Link>
-      </section>
-    </main>
+      </main>
+    </>
   );
 }
