8e03917e93f7bfdf849ff6d586fdbb380f0d3e6b
Yes2eul <yeseul5611@gmail.com>
2024-01-31 23:46:18 +0900

    feat: 홈화면 버튼 스타일 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
+import { Button } from "@nextui-org/react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex flex-col justify-center items-center">
       <main>Super Time Home</main>
-      <Link href="/auth/Login">
-        <button>login</button>
+      <Link href="/auth/login">
+        <Button className="bg-[#ffffff] border-solid border-1.5 border-main_blue text-main_blue">
+          로그인
+        </Button>
       </Link>
-      <Link href="/auth/SignUp">
-        <button>signUp</button>
+      <Link href="/auth/signup">
+        <Button className="bg-[#ffffff] border-solid border-1.5 border-main_blue text-main_blue">
+          슈퍼타임 회원가입
+        </Button>
       </Link>
     </div>
   );
