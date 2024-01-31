a0363879370390e0a3bc40588b0cb83e1f2bd505
Yes2eul <yeseul5611@gmail.com>
2024-02-01 01:30:31 +0900

    feat: 푸터 이미지 수정

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,16 +5,30 @@ export default function Footer() {
     <footer>
       <nav className="flex justify-center items-center">
         <Link href="/board/main">
-          <img src="./icons/coding.png" alt="전체 게시판" />
+          <img
+            src="/icons/coding.png"
+            width="45"
+            height="45"
+            alt="전체 게시판"
+          />
+          {/* "/icons/coding_color.png" */}
         </Link>
         <Link href="/board/community">
-          <img src="./icons/list.png" alt="커뮤니티 게시판" />
+          <img
+            src="/icons/list.png"
+            width="45"
+            height="45"
+            alt="커뮤니티 게시판"
+          />
+          {/* "/icons/list_color.png" */}
         </Link>
         <Link href="/chat">
-          <img src="./icons/chat.png" alt="채팅" />
+          <img src="/icons/chat.png" width="45" height="45" alt="채팅" />
+          {/* "/icons/chat_color.png" */}
         </Link>
         <Link href="/profile/user">
-          <img src="./icons/user.png" alt="프로필" />
+          <img src="/icons/user.png" width="45" height="45" alt="프로필" />
+          {/* "/icons/_color.png" */}
         </Link>
       </nav>
     </footer>
