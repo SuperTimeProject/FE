04ee8896a3fcaae8cdc4e7834b6f27e8b7d17622
chaeyeon <kcy671524@gmail.com>
2024-01-31 19:33:57 +0900

    기수 mock데이터 추가, 비밀번호 확인 수정, 닉네임,이메일 중복확인 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <main>Super Time Home</main>
+      <Link href="/auth/Login">
+        <button>login</button>
+      </Link>
+      <Link href="/auth/SignUp">
+        <button>signUp</button>
+      </Link>
     </div>
   );
 }
