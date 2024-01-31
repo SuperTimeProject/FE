60176bbeb9de991b0e2671660dbe8720baeb5751
Yes2eul <yeseul5611@gmail.com>
2024-02-01 00:20:24 +0900

    feat: 푸터 영역 수정

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,11 +4,11 @@ export default function Footer() {
   return (
     <footer>
       <nav className="flex justify-center items-center">
-        <Link href="/board/info">
-          <img src="./icons/coding.png" alt="정보" />
+        <Link href="/board/main">
+          <img src="./icons/coding.png" alt="전체 게시판" />
         </Link>
         <Link href="/board/community">
-          <img src="./icons/list.png" alt="게시판" />
+          <img src="./icons/list.png" alt="커뮤니티 게시판" />
         </Link>
         <Link href="/chat">
           <img src="./icons/chat.png" alt="채팅" />
