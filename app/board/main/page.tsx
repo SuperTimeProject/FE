940dadf05e283373bc9690fcd5e2d69cff1f5c5d
Yes2eul <yeseul5611@gmail.com>
2024-02-01 15:46:48 +0900

    feat: 푸터 영역 업데이트

--- app/board/main/page.tsx
+++ app/board/main/page.tsx
@@ -3,9 +3,11 @@ import Header from "@/components/header";
 
 export default function Main() {
   return (
-    <div className=" w-full h-screen flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center">
       <Header />
-      <main>전체 게시판 메인</main>
+      <div className="w-96 h-[600px] m-4 p-8 border-1 border-[#d1d5db] bg-white">
+        <main>전체 게시판 메인</main>
+      </div>
       <Footer />
     </div>
   );
