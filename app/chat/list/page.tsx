736588083f8fa268d28ea391a6bea87df733dfe4
Yes2eul <yeseul5611@gmail.com>
2024-02-01 16:03:49 +0900

    feat: 보드 업데이트

--- app/chat/list/page.tsx
+++ app/chat/list/page.tsx
@@ -1,12 +1,12 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
-export default function Community() {
+export default function List() {
   return (
     <div className="flex flex-col justify-center items-center">
       <Header />
       <div className="w-96 h-[600px] m-4 p-8 border-1 border-[#d1d5db] bg-white">
-        <main>커뮤니티 게시판 메인</main>
+        <main>채팅 리스트</main>
       </div>
       <Footer />
     </div>
--- app/profile/users/[id].tsx
+++ app/profile/users/[id].tsx
@@ -1,12 +1,12 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
-export default function Community() {
+export default function Users() {
   return (
     <div className="flex flex-col justify-center items-center">
       <Header />
       <div className="w-96 h-[600px] m-4 p-8 border-1 border-[#d1d5db] bg-white">
-        <main>커뮤니티 게시판 메인</main>
+        <main>프로필</main>
       </div>
       <Footer />
     </div>
