fc5c0bf990b4f8acd2a6d457431def135df0e190
Yes2eul <yeseul5611@gmail.com>
2024-02-01 14:00:58 +0900

    feat: css 스타일 변경, 아이콘 변경

--- app/auth/Login/page.tsx
+++ app/auth/Login/page.tsx
@@ -7,25 +7,27 @@ import { privateApi } from "@/api/axiosConfig";
 
 export default function Login() {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
-      <header className="m-10">로그인</header>
-      <main className="flex flex-col justify-center items-center gap-4">
-        <section className="flex flex-col gap-4">
-          <form className="flex flex-col gap-4">
-            <Input type="email" label="이메일" />
-            <Input type="password" label="비밀번호" />
-          </form>
-          <Button className="bg-main_blue text-white">로그인</Button>
-          <Button className="bg-sub_purple text-white">소셜 로그인</Button>
-        </section>
-      </main>
-      <footer className="m-10">
-        <Link href="/auth/signup">
-          <Button className="bg-[#ffffff] border-solid border-1.5 border-main_blue text-main_blue">
-            회원가입
-          </Button>
-        </Link>
-      </footer>
+    <div className="flex h-screen justify-center items-center">
+      <div className="w-96 p-8 border-1 border-[#d1d5db] bg-white shadow-md rounded-md">
+        <header className="flex justify-center text-3xl m-8">로그인</header>
+        <main className="flex flex-col gap-4">
+          <section className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4">
+              <Input type="email" label="이메일" />
+              <Input type="password" label="비밀번호" />
+            </form>
+            <Button className="bg-main_blue text-white">로그인</Button>
+            <Button className="bg-sub_purple text-white">소셜 로그인</Button>
+          </section>
+        </main>
+        <footer className="flex justify-center m-10">
+          <Link href="/auth/signup">
+            <Button className="bg-[#ffffff] border-solid border-1.5 border-main_blue text-main_blue">
+              회원가입
+            </Button>
+          </Link>
+        </footer>
+      </div>
     </div>
   );
 }
