b1f16f45e3d29490af8210d641027f4673bfaff3
Yes2eul <yeseul5611@gmail.com>
2024-02-01 14:56:32 +0900

    feat: css 추가

--- app/board/main/page.tsx
+++ app/board/main/page.tsx
@@ -3,10 +3,12 @@ import Header from "@/components/header";
 
 export default function Main() {
   return (
-    <div className=" w-full h-screen flex flex-col justify-center items-center">
-      <Header />
-      <main>전체 게시판 메인</main>
-      <Footer />
+    <div className="flex h-screen justify-center items-center">
+      <div className="w-96 p-8 border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
+        <Header />
+        <main>전체 게시판 메인</main>
+        <Footer />
+      </div>
     </div>
   );
 }
