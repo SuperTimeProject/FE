50331ea2e1369f7a9c999ba19fa1c750c09f7893
Yes2eul <yeseul5611@gmail.com>
2024-02-01 23:30:43 +0900

    feat: 메인 홈, 헤더 수정

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-center items-center">
-      <Link href="/">
-        <h1 className="p-4">Super Time</h1>
+      <Link href="/board/main">
+        <p className="text-3xl font-mono m-8">SUPER TIME</p>
       </Link>
     </header>
   );
