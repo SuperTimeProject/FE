75589220ee037a817e4b36cb6fbd7d13f5e3ffbd
Yes2eul <yeseul5611@gmail.com>
2024-02-02 16:37:32 +0900

    feat: root 페이지 업데이트

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,24 @@ import { Button } from "@nextui-org/react";
 import Link from "next/link";
 
 export default function Home() {
+  const textStyling = {
+    WebkitTextStroke: "1px black",
+  };
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <div className="w-96 p-8 border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
         <header className="flex flex-col items-center gap-8">
           <img src="/favicon.ico" width="128" height="128" />
-          <p className="text-5xl font-mono">SUPER TIME</p>
+          <div className="flex justify-center items-center m-4 gap-2">
+            <p style={textStyling} className="text-4xl font-mono text-white">
+              SUPER
+            </p>
+            <p style={textStyling} className="text-4xl font-mono text-white">
+              TIME
+            </p>
+          </div>
         </header>
-        <section className="flex flex-col items-center gap-8 mt-16">
+        <section className="flex flex-col items-center gap-8 mt-8">
           <Link href="/auth/login">
             <Button
               size="lg"
