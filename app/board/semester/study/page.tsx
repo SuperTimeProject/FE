8ab0b27b1a13be3fe8427cf71c7dd447d427cb92
Yes2eul <yeseul5611@gmail.com>
2024-02-02 13:43:55 +0900

    feat: css 스타일링 추가

--- app/board/semester/study/page.tsx
+++ app/board/semester/study/page.tsx
@@ -3,10 +3,14 @@ import Header from "@/components/header";
 
 export default function Study() {
   return (
-    <div className=" w-full h-screen flex flex-col justify-center items-center">
-      <Header />
-      <main>스터디 게시판 메인</main>
-      <Footer />
+    <div className="flex h-screen justify-center items-center">
+      <div className="max-w-[767px] flex flex-col items-center border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
+        <Header />
+        <div className="w-96 h-[600px] p-4 border-1 border-[#d1d5db] bg-white">
+          <main>스터디 게시판 메인</main>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
