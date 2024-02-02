c3fffb768f4f5a8cc5494324d4f349abe46f7a36
Yes2eul <yeseul5611@gmail.com>
2024-02-02 16:28:46 +0900

    feat: 게시물 작성 페이지 메인/커뮤니티 link 추가

--- app/board/community/page.tsx
+++ app/board/community/page.tsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Community() {
   return (
@@ -8,6 +10,20 @@ export default function Community() {
         <Header />
         <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">
           <main>커뮤니티 게시판 메인</main>
+          <Link href="/board/post/write">
+            <Button
+              isIconOnly
+              aria-label="post"
+              style={{ background: "#c4b0ff" }}
+            >
+              <img
+                src="/icons/post.png"
+                width="30"
+                height="30"
+                style={{ filter: "brightness(0) invert(1)" }} // 이미지 색 white로 변경
+              />
+            </Button>
+          </Link>
         </div>
         <Footer />
       </div>
--- app/board/post/write/page.tsx
+++ app/board/post/write/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { Button, Input } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Write() {
+  const [uploadFile, setUploadFile] = useState<File | null>(null);
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files !== null) {
+      const file = e.target.files[0];
+      setUploadFile(file);
+    } else {
+      setUploadFile(null);
+    }
+  };
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="max-w-[767px] flex flex-col items-center border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
@@ -13,11 +26,40 @@ export default function Write() {
               <p>게시물 작성</p>
               <form>
                 <Input type="text" label="제목"></Input>
-                <Input type="text" label="카테고리"></Input>
+                <Input type="text" label="게시판 선택? main/community"></Input>
+                {uploadFile && (
+                  <img
+                    src={URL.createObjectURL(uploadFile)}
+                    alt="Uploaded Preview"
+                    width="50"
+                    height="50"
+                    className="m-2 max-w-full"
+                  />
+                )}
+                <Button
+                  size="sm"
+                  className="bg-sub_purple font-semibold text-white"
+                >
+                  이미지 업로드
+                  <input
+                    type="file"
+                    accept="image/*"
+                    multiple // 이미지 multiple 선택
+                    onChange={handleImageUpload}
+                    style={{
+                      opacity: 0,
+                      position: "absolute",
+                    }}
+                  />
+                </Button>
                 <Input type="text" label="내용"></Input>
-                <Input type="file" accept="image/*" />
               </form>
-              <Button>게시</Button>
+              <Button
+                size="sm"
+                className="bg-sub_purple font-semibold text-white"
+              >
+                게시
+              </Button>
             </section>
           </main>
         </div>
