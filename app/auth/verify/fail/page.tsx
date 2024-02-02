090efd8ba9f15f5b8e8a9ec408086fd5fdf9ca81
Yes2eul <yeseul5611@gmail.com>
2024-02-02 15:54:32 +0900

    feat: 인증페이지 타이틀 업데이트

--- app/auth/verify/fail/page.tsx
+++ app/auth/verify/fail/page.tsx
@@ -7,6 +7,9 @@ import { useState } from "react";
 export default function Fail() {
   // "/auth/verify/fail" - 실패 처리 후 이미지 업로드로 재인증 요청
   // "/auth/verify/wait" - 대기 페이지로 이동
+  const textStyling = {
+    WebkitTextStroke: "1px black",
+  };
 
   const [uploadFile, setUploadFile] = useState<File | null>(null);
 
@@ -23,7 +26,14 @@ export default function Fail() {
     <div className="flex h-screen justify-center items-center">
       <div className="w-96 p-8 border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
         <header className="flex flex-col items-center">
-          <p className="text-3xl font-mono m-8">SUPER TIME</p>
+          <div className="flex justify-center items-center m-4 gap-2">
+            <p style={textStyling} className="text-4xl font-mono text-white">
+              SUPER
+            </p>
+            <p style={textStyling} className="text-4xl font-mono text-white">
+              TIME
+            </p>
+          </div>
           <p className="font-medium text-rose-600 text-xl m-4">
             기수 인증에 실패하였습니다.
           </p>
--- app/auth/verify/wait/page.tsx
+++ app/auth/verify/wait/page.tsx
@@ -6,13 +6,24 @@ export default function Wait() {
   // "/auth/verify/wait" - 요청 보낸 후 대기 상태
   // "/auth/verify/fail" 실패 - router.push("/auth/verify/fail");
   // "/auth/verify/success" 성공 - router.push("/auth/verify/success");
+  const textStyling = {
+    WebkitTextStroke: "1px black",
+  };
+
   const router = useRouter();
 
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="w-96 p-8 border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
         <header className="flex flex-col items-center">
-          <p className="text-3xl font-mono m-8">SUPER TIME</p>
+          <div className="flex justify-center items-center m-4 gap-2">
+            <p style={textStyling} className="text-4xl font-mono text-white">
+              SUPER
+            </p>
+            <p style={textStyling} className="text-4xl font-mono text-white">
+              TIME
+            </p>
+          </div>
           <img src="/icons/info.png" width="64" height="64" />
           <p className="font-medium text-xl m-4">인증 처리가 진행 중입니다.</p>
         </header>
