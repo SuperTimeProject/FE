d3a439016aa47714ba6b7d7101fe38b696f924a8
Yes2eul <yeseul5611@gmail.com>
2024-02-02 22:39:20 +0900

    feat: 게시글 작성 api 추가

--- app/board/post/write/page.tsx
+++ app/board/post/write/page.tsx
@@ -1,22 +1,87 @@
 "use client";
 
+import { publicApi } from "@/api/axiosConfig";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { Button, Divider, Input, Textarea } from "@nextui-org/react";
 import { useState } from "react";
 
+interface postInfo {
+  userCid: number;
+  postTitle: string;
+  postContent: string;
+  postImages: File[];
+}
+
 export default function Write() {
+  const [postInfo, setPostInfo] = useState<postInfo>({
+    userCid: 0,
+    postTitle: "",
+    postContent: "",
+    postImages: [],
+  });
   const [uploadFiles, setUploadFiles] = useState<File[]>([]);
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setPostInfo((prevInfo) => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+  };
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files) {
       const newFiles = Array.from(files).slice(0, 5);
-      if (newFiles.length + uploadFiles.length > 5) {
+      if (newFiles.length + postInfo.postImages.length > 5) {
         alert("이미지는 최대 5개까지 선택 가능합니다.");
       } else {
-        setUploadFiles((prevFiles) => [...prevFiles, ...newFiles]);
+        setPostInfo((prevInfo) => ({
+          ...prevInfo,
+          postImages: [...prevInfo.postImages, ...newFiles],
+        }));
       }
+    }
+  };
+
+  const handlePostSubmit = async () => {
+    try {
+      if (!postInfo.postTitle || !postInfo.postContent) {
+        alert("제목과 내용은 필수 입력 사항입니다.");
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("userCid", String(postInfo.userCid));
+      formData.append("postTitle", postInfo.postTitle);
+      formData.append("postContent", postInfo.postContent);
+
+      postInfo.postImages.forEach((image, index) => {
+        formData.append(`postImages[${index}]`, image);
+      });
+
+      const response = await publicApi.post(
+        "/Board/create/{boardCid}",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (response.data.success) {
+        alert("게시글 작성이 완료되었습니다.");
+        // 게시판 페이지로
+      } else {
+        alert("게시글 작성에 실패했습니다.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("서버 오류로 작성에 실패했습니다.");
     }
   };
 
@@ -26,11 +91,23 @@ export default function Write() {
         <Header />
         <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">
           <main className="flex flex-col gap-4">
-            <p className="flex justify-center">게시물 작성</p>
+            <p className="flex justify-center">게시글 작성</p>
             <form className="flex flex-col gap-4">
-              <Input type="text" label="제목"></Input>
+              <Input
+                type="text"
+                label="제목"
+                name="postTitle"
+                value={postInfo.postTitle}
+                onChange={handleInputChange}
+              />
               <Divider className="my-2" />
-              <Textarea placeholder="내용" className="h-[178px]" />
+              <Textarea
+                placeholder="내용"
+                name="postContent"
+                value={postInfo.postContent}
+                onChange={handleInputChange}
+                className="h-[178px]"
+              />
             </form>
             <div className="flex justify-end">
               <Button
@@ -47,12 +124,12 @@ export default function Write() {
                 />
               </Button>
             </div>
-            <section className="h-[120px] flex justify-center items-center">
-              {uploadFiles.map((file, index) => (
+            <section className="h-[120px] flex justify-start items-center">
+              {postInfo.postImages.map((file, index) => (
                 <img
                   key={index}
                   src={URL.createObjectURL(file)}
-                  alt={`Uploaded Preview ${index + 1}`}
+                  alt={`미리보기 ${index + 1}`}
                   className="m-1 w-16 h-16 object-cover"
                 />
               ))}
@@ -61,6 +138,7 @@ export default function Write() {
               <Button
                 size="sm"
                 className="bg-sub_purple font-semibold text-white"
+                onClick={handlePostSubmit}
               >
                 게시
               </Button>
