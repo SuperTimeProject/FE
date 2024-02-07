aedd681246bad0ccdf74c402153b78437ae25507
chaeyeon <kcy671524@gmail.com>
2024-02-08 03:17:37 +0900

    board-main 게시판 조회 추가

--- app/board/main/page.tsx
+++ app/board/main/page.tsx
@@ -1,9 +1,36 @@
+"use client";
+
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { privateApi } from "@/api/axiosConfig";
 
 export default function Main() {
+  const [boardData, setBoardData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchBoardData = async () => {
+      try {
+        const response = await privateApi.get("/board/getBoard/{boardCid}");
+        // 응답 처리
+        if (response.data.success) {
+          setBoardData(response.data.board);
+          setErrorMessage("");
+        } else {
+          setErrorMessage("게시판을 불러오는데 실패했습니다.");
+        }
+      } catch (error) {
+        console.error(error);
+        setErrorMessage("서버 오류로 게시판을 불러오는데 실패했습니다.");
+      }
+    };
+
+    fetchBoardData(); // 게시판 데이터 불러오기
+  }, []);
+
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="max-w-[767px] flex flex-col items-center border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
