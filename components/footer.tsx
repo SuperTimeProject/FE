94ada3511fa17d4d740285756efd523c8ac3d024
Yes2eul <yeseul5611@gmail.com>
2024-02-08 15:49:09 +0900

    마우스 hover 기능 추가

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,83 +4,77 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Footer() {
-  const [activeLink, setActiveLink] = useState<string | null>(null);
+  const [hoveredIcon, setHoveredIcon] = useState<string | null>(null);
 
-  const handleLinkClick = (link: string) => {
-    setActiveLink(link);
+  const handleIconHover = (icon: string | null) => {
+    setHoveredIcon(icon);
   };
-
-  const isActive = (link: string) => link === activeLink;
 
   return (
     <footer>
       <nav className="flex justify-center items-center">
         <ul className="flex space-x-16 p-4">
           <li>
-            <Link
-              href="/board/main"
-              onClick={() => handleLinkClick("/board/main")}
-            >
+            <Link href="/board/main">
               <img
                 src={
-                  isActive("/board/main")
+                  hoveredIcon === "coding"
                     ? "/icons/coding_color.png"
                     : "/icons/coding.png"
                 }
                 width="40"
                 height="40"
                 alt="전체 게시판"
+                onMouseEnter={() => handleIconHover("coding")}
+                onMouseLeave={() => handleIconHover(null)}
               />
             </Link>
           </li>
           <li>
-            <Link
-              href="/board/community"
-              onClick={() => handleLinkClick("/board/community")}
-            >
+            <Link href="/board/community">
               <img
                 src={
-                  isActive("/board/community")
+                  hoveredIcon === "list"
                     ? "/icons/list_color.png"
                     : "/icons/list.png"
                 }
                 width="40"
                 height="40"
                 alt="커뮤니티 게시판"
+                onMouseEnter={() => handleIconHover("list")}
+                onMouseLeave={() => handleIconHover(null)}
               />
             </Link>
           </li>
           <li>
-            <Link
-              href="/chat/list"
-              onClick={() => handleLinkClick("/chat/list")}
-            >
+            <Link href="/chat">
               <img
                 src={
-                  isActive("/chat/list")
+                  hoveredIcon === "chat"
                     ? "/icons/chat_color.png"
                     : "/icons/chat.png"
                 }
                 width="40"
                 height="40"
                 alt="채팅"
+                onMouseEnter={() => handleIconHover("chat")}
+                onMouseLeave={() => handleIconHover(null)}
               />
             </Link>
           </li>
           <li>
-            <Link
-              href="/profile/users"
-              onClick={() => handleLinkClick("/profile/users")}
-            >
+            <Link href="/profile/users">
               <img
                 src={
-                  isActive("/profile/users")
-                    ? "/icons/_color.png"
+                  hoveredIcon === "user"
+                    ? "/icons/user_color.png"
                     : "/icons/user.png"
                 }
                 width="40"
                 height="40"
                 alt="프로필"
+                onMouseEnter={() => handleIconHover("user")}
+                onMouseLeave={() => handleIconHover(null)}
               />
             </Link>
           </li>
