2df8b099c080f51fe767a494f72bdf0f6dec197c
chaeyeon <kcy671524@gmail.com>
2024-02-15 15:00:12 +0900

    BASE_URL 수정

--- api/axiosConfig.tsx
+++ api/axiosConfig.tsx
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 //const BASE_URL = "http://54.180.122.9:8080/";
-const BASE_URL = "http://54.180.152.105:8080/";
+//const BASE_URL = "http://54.180.152.105:8080/";
+const BASE_URL = "http://3.37.61.223:8080/";
 
 export const publicApi: AxiosInstance = axios.create({
   baseURL: BASE_URL,
