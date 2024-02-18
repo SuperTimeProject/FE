53a2049cefa6a54a4555c7a2bf2c6822f300eca3
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-02-18 23:41:20 +0900

    fix: 데이터 전송 방법 수정
    닉네임 부분은  formdata가 아닌 params로 전달이 되도록 수정하였습니다.

--- app/profile/[userCid]/page.tsx
+++ app/profile/[userCid]/page.tsx
@@ -114,34 +114,27 @@ export default function Users() {
         return;
       }
 
-      const nicknameCheck = await publicApi.get(
-        "/auth/duplicateTest/nickname",
-        {
-          params: {
-            nickname: editInfo.userNickname,
-          },
-        }
-      );
+      const nicknameCheck = await publicApi.get("/auth/duplicateTest/nickname", {
+        params: {
+          nickname: editInfo.userNickname,
+        },
+      });
 
       if (nicknameCheck.data.duplicate) {
         alert("이미 사용 중인 닉네임입니다.");
         return;
       }
 
-      const editData = { userNickname: editInfo.userNickname || "" };
-      const editInfoJson = JSON.stringify(editData);
-      const editInfoBlob = new Blob([editInfoJson], {
-        type: "application/json",
-      });
+      const params = {
+        userNickname: editInfo.userNickname || "",
+      };
 
       const formData = new FormData();
-      formData.append("editInfo", editInfoBlob);
-
       if (editInfo.userProfileImage !== null) {
-        formData.append("profileImage", editInfo.userProfileImage);
+        formData.append("userProfileImage", editInfo.userProfileImage);
       }
 
-      const response = await privateApi.put("/user/info/edit", formData);
+      const response = await privateApi.put("/user/info/edit", formData, { params });
 
       if (response.data.success) {
         const updatedUserRes = await privateApi.get("/auth/getUserInfo");
@@ -184,9 +177,7 @@ export default function Users() {
 
   const handleImageDelete = async () => {
     try {
-      const response = await privateApi.delete(
-        "/user/info/profileImage/delete"
-      );
+      const response = await privateApi.delete("/user/info/profileImage/delete");
       if (response.data.success) {
         alert("프로필 사진이 삭제되었습니다.");
         window.location.reload();
@@ -233,9 +224,7 @@ export default function Users() {
         <Header />
         <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">
           <main>
-            <p className="flex justify-center text-xl font-mono font-semibold m-4">
-              마이페이지
-            </p>
+            <p className="flex justify-center text-xl font-mono font-semibold m-4">마이페이지</p>
             <ul className="flex flex-col gap-4">
               <li className="flex justify-evenly items-center p-2">
                 <div className="relative">
@@ -251,26 +240,15 @@ export default function Users() {
                   />
 
                   {isProfileEditMode && (
-                    <Button
-                      isIconOnly
-                      size="sm"
-                      className="absolute bottom-0 right-0 bg-white"
-                    >
-                      <input
-                        type="file"
-                        accept="image/*"
-                        onChange={handleImageUpload}
-                        className="opacity-0 absolute"
-                      />
+                    <Button isIconOnly size="sm" className="absolute bottom-0 right-0 bg-white">
+                      <input type="file" accept="image/*" onChange={handleImageUpload} className="opacity-0 absolute" />
                       <img src="/icons/photo.png" className="w-6 h-6" />
                     </Button>
                   )}
                 </div>
                 <div className="flex flex-col justify-center items-center gap-2">
                   <div className="flex justify-center items-center gap-2">
-                    <p className="font-mono">
-                      {userInfo?.semester.semesterDetailName}
-                    </p>
+                    <p className="font-mono">{userInfo?.semester.semesterDetailName}</p>
                     <Dropdown>
                       <DropdownTrigger>
                         <Button size="sm" variant="ghost">
@@ -279,19 +257,14 @@ export default function Users() {
                       </DropdownTrigger>
                       <DropdownMenu>
                         {partOptions.map((part) => (
-                          <DropdownItem
-                            key={part}
-                            onClick={() => handlePartSelect(part)}
-                          >
+                          <DropdownItem key={part} onClick={() => handlePartSelect(part)}>
                             {part}
                           </DropdownItem>
                         ))}
                       </DropdownMenu>
                     </Dropdown>
                   </div>
-                  <p className="text-xs text-red-500">
-                    *주특기를 선택해주세요.
-                  </p>
+                  <p className="text-xs text-red-500">*주특기를 선택해주세요.</p>
                 </div>
               </li>
               <li>
@@ -320,11 +293,7 @@ export default function Users() {
                     type="text"
                     label="닉네임"
                     variant="underlined"
-                    value={
-                      isProfileEditMode
-                        ? editInfo.userNickname
-                        : userInfo?.userNickname
-                    }
+                    value={isProfileEditMode ? editInfo.userNickname : userInfo?.userNickname}
                     onChange={(e) =>
                       setEditInfo((prevEditInfo) => ({
                         ...prevEditInfo,
@@ -338,23 +307,13 @@ export default function Users() {
               <div className="flex justify-end items-center mb-4">
                 {isProfileEditMode ? (
                   <>
-                    <p className="text-xs text-red-500 pr-4">
-                      *프로필 사진과 닉네임만 변경 가능합니다.
-                    </p>
-                    <Button
-                      size="sm"
-                      onClick={editSubmit}
-                      className="bg-sub_purple text-white"
-                    >
+                    <p className="text-xs text-red-500 pr-4">*프로필 사진과 닉네임만 변경 가능합니다.</p>
+                    <Button size="sm" onClick={editSubmit} className="bg-sub_purple text-white">
                       변경
                     </Button>
                   </>
                 ) : (
-                  <Button
-                    size="sm"
-                    onClick={handleProfileEdit}
-                    className="bg-sub_purple text-white"
-                  >
+                  <Button size="sm" onClick={handleProfileEdit} className="bg-sub_purple text-white">
                     프로필 수정
                   </Button>
                 )}
@@ -386,9 +345,7 @@ export default function Users() {
                   <ModalContent>
                     {(onClose) => (
                       <>
-                        <ModalHeader className="flex flex-col gap-1">
-                          회원탈퇴
-                        </ModalHeader>
+                        <ModalHeader className="flex flex-col gap-1">회원탈퇴</ModalHeader>
                         <ModalBody>
                           <p>회원탈퇴하시겠습니까?</p>
                           <p>탈퇴시, 계정은 삭제되며 복구되지 않습니다.</p>
@@ -397,11 +354,7 @@ export default function Users() {
                           <Button variant="light" onPress={onClose}>
                             취소
                           </Button>
-                          <Button
-                            color="danger"
-                            onPress={onClose}
-                            onClick={() => handleDeleteAccount()}
-                          >
+                          <Button color="danger" onPress={onClose} onClick={() => handleDeleteAccount()}>
                             탈퇴
                           </Button>
                         </ModalFooter>
