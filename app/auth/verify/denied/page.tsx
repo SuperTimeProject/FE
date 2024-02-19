50f387258bf5b68ef9da02371bedd463edd142b5
Yes2eul <yeseul5611@gmail.com>
2024-02-19 18:04:08 +0900

    feat: 인증페이지 업데이트

--- app/auth/verify/denied/page.tsx
+++ app/auth/verify/denied/page.tsx
@@ -4,13 +4,7 @@ import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { useState } from "react";
 
-export default function Fail() {
-  // "/auth/verify/fail" - 실패 처리 후 이미지 업로드로 재인증 요청
-  // "/auth/verify/wait" - 대기 페이지로 이동
-  const textStyling = {
-    WebkitTextStroke: "1px black",
-  };
-
+export default function Denied() {
   const [uploadFile, setUploadFile] = useState<File | null>(null);
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,12 +21,8 @@ export default function Fail() {
       <div className="w-96 p-8 border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
         <header className="flex flex-col items-center">
           <div className="flex justify-center items-center m-4 gap-2">
-            <p style={textStyling} className="text-4xl font-mono text-white">
-              SUPER
-            </p>
-            <p style={textStyling} className="text-4xl font-mono text-white">
-              TIME
-            </p>
+            <p className="text-4xl font-mono">SUPER</p>
+            <p className="text-4xl font-mono">TIME</p>
           </div>
           <p className="font-medium text-rose-600 text-xl m-4">
             기수 인증에 실패하였습니다.
@@ -44,7 +34,7 @@ export default function Fail() {
               <img
                 src={URL.createObjectURL(uploadFile)}
                 alt="Uploaded Preview"
-                className="m-2 w-64 h-64"
+                className="m-2"
               />
             )}
             <Button
@@ -60,7 +50,7 @@ export default function Fail() {
               />
             </Button>
           </section>
-          <Link href="/auth/verify/wait">
+          <Link href="/auth/verify/pending">
             <Button
               size="sm"
               className="bg-[#ffffff] border-solid border-1.5 border-main_blue text-main_blue"
--- app/auth/verify/needed/page.tsx
+++ app/auth/verify/needed/page.tsx
@@ -4,13 +4,7 @@ import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { useState } from "react";
 
-export default function Request() {
-  // "/auth/verify/request" - 로그인 후 처음 인증 요청
-  // "/auth/verify/wait" - 대기 페이지로 이동
-  const textStyling = {
-    WebkitTextStroke: "1px black",
-  };
-
+export default function Needed() {
   const [uploadFile, setUploadFile] = useState<File | null>(null);
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,12 +21,8 @@ export default function Request() {
       <div className="w-96 p-8 border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
         <header className="flex flex-col justify-center items-center">
           <div className="flex justify-center items-center m-4 gap-2">
-            <p style={textStyling} className="text-4xl font-mono text-white">
-              SUPER
-            </p>
-            <p style={textStyling} className="text-4xl font-mono text-white">
-              TIME
-            </p>
+            <p className="text-4xl font-mono">SUPER</p>
+            <p className="text-4xl font-mono">TIME</p>
           </div>
           <p className="font-medium text-xl m-4">기수 인증</p>
         </header>
@@ -42,7 +32,7 @@ export default function Request() {
               <img
                 src={URL.createObjectURL(uploadFile)}
                 alt="Uploaded Preview"
-                className="m-2 w-64 h-64"
+                className="m-2"
               />
             )}
             <Button
@@ -59,7 +49,7 @@ export default function Request() {
             </Button>
           </section>
 
-          <Link href="/auth/verify/wait">
+          <Link href="/auth/verify/pending">
             <Button
               size="sm"
               className="bg-[#ffffff] border-solid border-1.5 border-main_blue text-main_blue"
