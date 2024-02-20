cd7a9a62d9dcc6c15be7028399f4b7a965db8628
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-02-20 13:58:52 +0900

    feat: 시간표 이미지 수정

--- app/board/community/page.tsx
+++ app/board/community/page.tsx
@@ -21,8 +21,10 @@ interface ScheduleImageData {
 
 export default function Community() {
   const [userBoard, setUserBoard] = useState<UserBoard[]>([]);
-  const [scheduleImages, setScheduleImages] = useState<ScheduleImageData[]>([]);
+  const [scheduleImages, setScheduleImages] = useState<ScheduleImageData>();
   const [errorMessage, setErrorMessage] = useState("");
+  const [part, setPart] = useState<string>("");
+  const [isFull, setIsFull] = useState<string>("");
 
   /*   useEffect(() => {
     const getUser = async () => {
@@ -40,67 +42,70 @@ export default function Community() {
       const res = await privateApi.get("/auth/getUserInfo");
       if (res.data.success) {
         const userBoardData: UserBoard[] = res.data.getUserInfo.boardList;
-        const filteredUserBoardData = userBoardData.filter(
-          (board) => board.boardCid !== 1
-        );
+        const filteredUserBoardData = userBoardData.filter((board) => board.boardCid !== 1);
         setUserBoard(filteredUserBoardData);
+        setPart(res.data.getUserInfo.part);
+        setIsFull(res.data.getUserInfo.semester.isFull);
       }
     };
 
+    getUser();
+  }, []);
+
+  useEffect(() => {
     const fetchScheduleImages = async () => {
       try {
+        console.log(part, isFull);
         const scheduleResponse = await privateApi.get("/schedule/view", {
           params: {
-            part: "",
-            isFull: "",
+            part,
+            isFull,
             week: 1,
           },
         });
         if (scheduleResponse.data.success) {
-          setScheduleImages(scheduleResponse.data.scheduleImages);
+          setScheduleImages(scheduleResponse.data.image);
         }
       } catch (error) {
         if (axios.isAxiosError(error)) {
-          setScheduleImages([]);
+          if (error.status === 400) {
+            setScheduleImages(undefined);
+          }
           setErrorMessage(error.response?.data.message);
         }
       }
     };
 
-    getUser();
     fetchScheduleImages();
-  }, []);
+  }, [part, isFull]);
 
   return (
     <div className="flex h-screen justify-center items-center ">
-      <div className="max-w-[767px] flex flex-col items-center border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
+      <div className=" max-w-[767px] flex flex-col items-center border-1 border-[#d1d5db] bg-white shadow-lg rounded-lg">
         <Header />
-        {/* 게시판 리스트 -> UserBoard - 전체 게시판 제외 */}
-        <div className="h-[200px]">
-          {userBoard.map((board) => (
-            <div className="w-96 flex m-2 p-2 pr-4 pl-4 justify-between items-center border-b-2">
-              <div>{board.boardName}</div>
-              <Link href={`/board/community/${board.boardName}`}>
-                <div>+ 더보기</div>
-              </Link>
-            </div>
-          ))}
-        </div>
-        {/* PostExample 호출  */}
+        <div className=" h-[600px] border-y-1 border-[#d1d5db] flex flex-col items-center">
+          {/* 게시판 리스트 -> UserBoard - 전체 게시판 제외 */}
+          <div className="h-[300px]">
+            {userBoard.map((board) => (
+              <div className="w-96 flex m-2 p-2 pr-4 pl-4 justify-between items-center border-b-2" key={board.boardCid}>
+                <div>{board.boardName}</div>
+                <Link href={`/board/community/${board.boardName}`}>
+                  <div>+ 더보기</div>
+                </Link>
+              </div>
+            ))}
+          </div>
+          {/* PostExample 호출  */}
 
-        {/* 시간표 */}
-        <div className="h-[200px]">
-          {scheduleImages.map((image) => (
-            <div key={image.scheduleImageCid} className="m-2">
-              <img
-                src={image.scheduleImageFilePath}
-                alt={image.scheduleImageFileName}
-                className="max-w-full h-auto"
-              />
-            </div>
-          ))}
+          {/* 시간표 */}
+          <div className=" h-[0.2px] border-t-1 border-[#d1d5db] w-full my-1"></div>
+          <h2 className=" font-semibold">시간표</h2>
+          <div className=" h-[0.2px] border-t-1 border-[#d1d5db] w-full my-1"></div>
+          <div
+            className=" h-[250px] w-[380px] bg-cover bg-center"
+            style={{ backgroundImage: `url(${scheduleImages?.scheduleImageFilePath})` }}
+          />
         </div>
-
         <Footer />
       </div>
     </div>
--- components/post/boardComponent.tsx
+++ components/post/boardComponent.tsx
@@ -4,6 +4,7 @@ import { privateApi } from "@/api/axiosConfig";
 import { Button, Pagination } from "@nextui-org/react";
 import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 interface UserBoard {
@@ -20,6 +21,8 @@ interface IPostInfo {
 }
 
 export default function BoardComponent({ boardName }: { boardName: string }) {
+  const route = useRouter();
+
   const [boardData, setBoardData] = useState<IPostInfo[] | null>([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [page, setPage] = useState<number>(1);
@@ -31,13 +34,7 @@ export default function BoardComponent({ boardName }: { boardName: string }) {
       const res = await privateApi.get("/auth/getUserInfo");
       if (res.data.success) {
         const userBoardData = res.data.getUserInfo.boardList;
-        setUserBoard(
-          userBoardData[
-            userBoardData.findIndex(
-              (board: UserBoard) => board.boardName === boardName
-            )
-          ]
-        );
+        setUserBoard(userBoardData[userBoardData.findIndex((board: UserBoard) => board.boardName === boardName)]);
       }
     };
     getUser();
@@ -47,9 +44,7 @@ export default function BoardComponent({ boardName }: { boardName: string }) {
     if (userBoard === null) return;
     const fetchBoardData = async () => {
       try {
-        const response = await privateApi.get(
-          `/board/getBoard/${userBoard.boardCid}/${page}`
-        );
+        const response = await privateApi.get(`/board/getBoard/${userBoard.boardCid}/${page}`);
         if (response.data.success) {
           setBoardData(response.data.postList);
           setErrorMessage("");
@@ -66,33 +61,25 @@ export default function BoardComponent({ boardName }: { boardName: string }) {
   }, [page, userBoard]);
 
   if (userBoard === null)
-    return (
-      <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">
-        로딩중..
-      </div>
-    );
+    return <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">로딩중..</div>;
 
   return (
     <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">
       <main className="pb-2 flex justify-between text-xl tracking-widest items-center border-b-2 border-gray-700 pl-1 pr-1">
         {/* {userBoard.boardName} */}
         {boardName}
-        <Link href="/board/post/create">
-          <Button
-            isIconOnly
-            aria-label="post"
-            className="bg-sub_purple float-right"
-          >
-            <img
-              src="/icons/post.png"
-              width="25"
-              height="25"
-              style={{ filter: "brightness(0) invert(1)" }}
-            />
-          </Button>
-        </Link>
+        <div className=" flex justify-center items-center space-x-3 ">
+          <div className="flex-none cursor-pointer" onClick={() => route.back()}>
+            <img src="/icons/back.png" width="30" height="30" className="flex-none" />
+          </div>
+          <Link href="/board/post/create">
+            <Button isIconOnly aria-label="post" className="bg-sub_purple float-right">
+              <img src="/icons/post.png" width="25" height="25" style={{ filter: "brightness(0) invert(1)" }} />
+            </Button>
+          </Link>
+        </div>
       </main>
-      <div className="h-[450px] overflow-auto scrollbar-none">
+      <div className="h-[450px] overflow-auto scrollbar-none mt-3">
         {errorMessage && <p>{errorMessage}</p>}
         {boardData &&
           boardData.map((post) => (
