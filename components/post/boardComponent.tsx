c9388114e889a9f0fc3c1ea28e4447b71ccef3a8
chaeyeon <kcy671524@gmail.com>
2024-02-20 14:23:26 +0900

    게시판 위 아이콘 정렬 수정

--- components/post/boardComponent.tsx
+++ components/post/boardComponent.tsx
@@ -34,7 +34,13 @@ export default function BoardComponent({ boardName }: { boardName: string }) {
       const res = await privateApi.get("/auth/getUserInfo");
       if (res.data.success) {
         const userBoardData = res.data.getUserInfo.boardList;
-        setUserBoard(userBoardData[userBoardData.findIndex((board: UserBoard) => board.boardName === boardName)]);
+        setUserBoard(
+          userBoardData[
+            userBoardData.findIndex(
+              (board: UserBoard) => board.boardName === boardName
+            )
+          ]
+        );
       }
     };
     getUser();
@@ -44,7 +50,9 @@ export default function BoardComponent({ boardName }: { boardName: string }) {
     if (userBoard === null) return;
     const fetchBoardData = async () => {
       try {
-        const response = await privateApi.get(`/board/getBoard/${userBoard.boardCid}/${page}`);
+        const response = await privateApi.get(
+          `/board/getBoard/${userBoard.boardCid}/${page}`
+        );
         if (response.data.success) {
           setBoardData(response.data.postList);
           setErrorMessage("");
@@ -61,20 +69,44 @@ export default function BoardComponent({ boardName }: { boardName: string }) {
   }, [page, userBoard]);
 
   if (userBoard === null)
-    return <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">로딩중..</div>;
+    return (
+      <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">
+        로딩중..
+      </div>
+    );
 
   return (
     <div className="w-96 h-[600px] m-2 p-4 border-1 border-[#d1d5db] bg-white">
-      <main className="pb-2 flex justify-between text-xl tracking-widest items-center border-b-2 border-gray-700 pl-1 pr-1">
+      <main className="pb-2 text-xl tracking-widest border-b-2 border-gray-700 pl-1 pr-1">
         {/* {userBoard.boardName} */}
-        {boardName}
-        <div className=" flex justify-center items-center space-x-3 ">
-          <div className="flex-none cursor-pointer" onClick={() => route.back()}>
-            <img src="/icons/back.png" width="30" height="30" className="flex-none" />
-          </div>
+        {/* {boardName} */}
+        <div className="flex justify-between items-center">
+          {boardName !== "전체 게시판" && (
+            <div
+              className="flex-none cursor-pointer"
+              onClick={() => route.back()}
+            >
+              <img
+                src="/icons/back.png"
+                width="30"
+                height="30"
+                className="flex-none"
+              />
+            </div>
+          )}
+          <div>{boardName}</div>
           <Link href="/board/post/create">
-            <Button isIconOnly aria-label="post" className="bg-sub_purple float-right">
-              <img src="/icons/post.png" width="25" height="25" style={{ filter: "brightness(0) invert(1)" }} />
+            <Button
+              isIconOnly
+              aria-label="post"
+              className="bg-sub_purple float-right"
+            >
+              <img
+                src="/icons/post.png"
+                width="25"
+                height="25"
+                style={{ filter: "brightness(0) invert(1)" }}
+              />
             </Button>
           </Link>
         </div>
